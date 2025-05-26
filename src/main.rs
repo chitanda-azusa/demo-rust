5094a6df41fd10915575b5b0ab458d1f8f9b6859
chitanda-azusa <yifan.cao@smartnews.com>
2025-05-26 13:08:27 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+use crate::handlers::hello_world::hello_world;
+
+pub mod handlers;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+    let listener = tokio::net::TcpListener::bind("localhost:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
